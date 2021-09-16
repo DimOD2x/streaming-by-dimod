e37b26a4d1515136917a47264f823a3394d1d58c
DimOD2x <89885606+DimOD2x@users.noreply.github.com>
2021-09-16 13:14:28 +0300

    Update dimod.js

--- dimod.js
+++ dimod.js
@@ -1,1 +1,29 @@
 
+const Discord = require("discord.js");
+const dimod = new Discord.Client();
+//////// playing or dnd  streaming ////////
+dimod.on("ready", () => {
+  console.log(
+    `Online In Servers`
+  );
+  let statuses = [
+  
+  `D1MOD.`,
+ 
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    dimod.user.setActivity(STREAMING, {      
+    type: "STREAMING", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+///////////////////////
+dimod.on("ready", () => {
+    var join = dimod.channels.get(""); /// id voice
+    if (join) join.join();
+  });
+//////////////// tokn lera da bne 
+dimod.login("");
+
